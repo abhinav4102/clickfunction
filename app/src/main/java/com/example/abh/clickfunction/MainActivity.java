6aa3a1b2a3e4d1c1d8bcf8faa716f34be5a470ca
Abhi <abhinavreddy2014@gmail.com>
2017-08-24 12:05:13 -0400

    trial version

---
+++ app/src/main/java/com/example/abh/clickfunction/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.abh.clickfunction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public void clickfunction(View view){
+        Log.i("Info","checked");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
