a65b99f8669bbd98da3e8b64f7da5114daae032d
Abhi <abhinavreddy2014@gmail.com>
2017-08-24 12:12:02 -0400

    Added changes

--- app/src/main/java/com/example/abh/clickfunction/MainActivity.java
+++ app/src/main/java/com/example/abh/clickfunction/MainActivity.java
@@ -14,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String abh = "abh";
+
+
+        
     }
 }
